404566762f08b5c350e7e7ed6a8125ab36511063
tospectre <fhaiemrachid@gmail.com>
2018-03-12 11:26:13 +0100

    Add two other tests for primeAnnuelle methode

--- src/test/java/com/ipiecoles/java/java350/model/CommercialTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/CommercialTest.java
@@ -17,4 +17,31 @@ public class CommercialTest {
         //Then
         Assertions.assertThat(prime).isEqualTo(500d);
     }
+
+    @Test
+    public void testPrimeAnnuelleWithCA0(){
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(0d);
+
+        //When
+        Double prime = commercial.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertThat(prime).isEqualTo(500d);
+    }
+
+    @Test
+    public void testPrimeAnnuelleWithCA100000(){
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(100000d);
+
+        //When
+        Double prime = commercial.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertThat(prime).isEqualTo(5000d);
+    }
+
 }
