91c24bfe9f9d60db7f42ddd5fdab7871bd63553b
tospectre <fhaiemrachid@gmail.com>
2018-03-12 10:43:39 +0100

    added Unit test folder

---
+++ src/test/java/com/ipiecoles/java/java350/model/CommercialTest.java
@@ -0,0 +1,20 @@
+package com.ipiecoles.java.java350.model;
+
+import org.junit.Test;
+import org.assertj.core.api.Assertions;
+
+public class CommercialTest {
+
+    @Test
+    public void testPrimeAnnuelleWithCAnull() {
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(null);
+
+        //When
+        Double prime = commercial.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertThat(prime).isEqualTo(560d);
+    }
+}
